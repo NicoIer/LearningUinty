0f909a58e6b5885237bf2fc30d63d9943350bdd5
ChenJun <NicoIer@163.com>
2022-12-24 13:03:50 +0800

    未来的对话框

---
+++ Assets/Games/AttackGame/script/ui/FloatPanel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AttackGame.script.ui
+{
+    public class FloatPanel : MonoBehaviour
+    {
+        public void SetPosition(Vector3 pos)
+        {
+            transform.localPosition = pos;
+        }
+    }
+}
