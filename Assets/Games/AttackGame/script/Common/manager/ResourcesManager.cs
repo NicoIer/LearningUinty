9287e1997d04a0e36de9317cae868390a901778e
ChenJun <NicoIer@163.com>
2022-12-30 19:45:01 +0800

    代码结构调整(文件移动)

--- Assets/Games/AttackGame/script/Common/manager/ResourcesManager.cs
+++ Assets/Games/AttackGame/script/Common/manager/ResourcesManager.cs
@@ -4,6 +4,14 @@ namespace AttackGame.Common.Manager
 {
     public static class ResourcesManager
     {
+        //ToDo 使用它 读取/存储 信息
 
+        public static void Load<T>(string path)
+        {
+        }
+
+        public static void Save<T>(string path)
+        {
+        }
     }
 }
